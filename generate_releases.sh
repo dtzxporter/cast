ba0f8d44507a870d1aad527e759157626aac0409
dtzxporter <dtzxporter@users.noreply.github.com>
2023-09-21 16:19:17 -0400

    Update generate_releases.sh

--- generate_releases.sh
+++ generate_releases.sh
@@ -9,5 +9,6 @@ cp -r ./libraries/python/* ./.releases/io_scene_cast/
 cp -r ./plugins/maya/* ./.releases/maya/
 cp -r ./libraries/python/* ./.releases/maya/
 
+# TODO: This packages the parent folders with the zip files.
 zip -r -q ./.releases/upload/blender_cast_plugin.zip ./.releases/io_scene_cast/
 zip -r -q ./.releases/upload/maya_cast_plugin.zip ./.releases/maya/
