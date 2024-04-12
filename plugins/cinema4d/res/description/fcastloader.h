59708bab70f08ebf1671c8c131d0657d96acc15d
Devo <devomvm@gmail.com>
2024-04-12 14:55:51 +0200

    Cinema 4D Cast Support
    Cast Model support for Cinema 4D.
    
    Supports:
    - Meshes
    - Skeleton
    - Materials
    - Vertex colors
    - Constraints
    - IK handles
    
    Installation:
    Create a folder with the name "Cast", "io_scene_cast" or any other suitable name and place the files in there. The cast python library (cast.py) should be placed in the resource folder named "res" in the plugins directory.
    
    Requirements:
    Cinema 4D 2024 or newer
    
    Currently not supported:
    - Animations
    - Instances
    - Blend shapes

---
+++ plugins/cinema4d/res/description/fcastloader.h
@@ -0,0 +1,15 @@
+#ifndef _FCASTLOADER_H__
+#define _FCASTLOADER_H__
+
+enum 
+{
+	
+	fies_loader = 10000,
+	CAST_IMPORT_CONSTRAINTS,
+	CAST_IMPORT_IK_HANDLES,
+	CAST_IMPORT_BIND_SKIN,
+	CAST_IMPORT_GROUP
+
+};
+#endif
+
