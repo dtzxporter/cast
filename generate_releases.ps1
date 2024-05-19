379399d43547e55930490b9cfe4de247f316a580
Huba Tuba <57007485+floxay@users.noreply.github.com>
2024-05-19 04:12:27 +0200

    adjust release generator scripts

--- generate_releases.ps1
+++ generate_releases.ps1
@@ -4,9 +4,7 @@ New-Item -Force -Path "./.releases" -Name "maya" -ItemType "directory"
 New-Item -Force -Path "./.releases" -Name "upload" -ItemType "directory"
 
 Copy-item -Force -Recurse -Verbose "./plugins/blender/*" "./.releases/io_scene_cast/"
-Copy-item -Force -Recurse -Verbose "./libraries/python/*" "./.releases/io_scene_cast/"
 Copy-item -Force -Recurse -Verbose "./plugins/maya/*" "./.releases/maya/"
-Copy-item -Force -Recurse -Verbose "./libraries/python/*" "./.releases/maya/"
 
 Compress-Archive -Force -Path "./.releases/io_scene_cast" -DestinationPath "./.releases/upload/blender_cast_plugin.zip"
 Compress-Archive -Force -Path "./.releases/maya/*.py" -DestinationPath "./.releases/upload/maya_cast_plugin.zip"
--- generate_releases.sh
+++ generate_releases.sh
@@ -5,9 +5,7 @@ mkdir -p ./.releases/maya
 mkdir -p ./.releases/upload
 
 cp -r ./plugins/blender/* ./.releases/io_scene_cast/
-cp -r ./libraries/python/* ./.releases/io_scene_cast/
 cp -r ./plugins/maya/* ./.releases/maya/
-cp -r ./libraries/python/* ./.releases/maya/
 
 ditto -c -k --sequesterRsrc --keepParent ./.releases/io_scene_cast/ ./.releases/upload/blender_cast_plugin.zip
 ditto -c -k --sequesterRsrc ./.releases/maya/ ./.releases/upload/maya_cast_plugin.zip
