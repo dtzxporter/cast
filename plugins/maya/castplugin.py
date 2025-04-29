3c56523fa409eb718b1d48cb3cda2b492e02b7a4
dtzxporter <dtzxporter@users.noreply.github.com>
2025-04-29 10:38:18 -0400

    Set recommended ior if specular color is provided in maya.

--- plugins/maya/castplugin.py
+++ plugins/maya/castplugin.py
@@ -42,7 +42,7 @@ sceneSettings = {
 }
 
 # Shared version number
-version = "1.81"
+version = "1.82"
 
 # Time unit to framerate map
 framerateMap = {
@@ -685,7 +685,8 @@ def utilityAssignMaterialSlots(shader, slots, basic, path):
     else:
         # Set reasonable defaults for specular/gloss workflow.
         cmds.setAttr("%s.metalness" % shader, 0.0)
-        cmds.setAttr("%s.specularIOR" % shader, 1.5)
+        cmds.setAttr("%s.specularIOR" %
+                     shader, 1000 if "specular" in slots else 1.5)
 
         switcher = {
             "albedo": "baseColor",
